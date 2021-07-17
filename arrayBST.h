adbe8607d9acc5ce4b5be5b2b012f72168fcd1e0
Ankush Niroula <ankushniroulaa@gmail.com>
2021-07-17 22:55:48 +0545

    without remove

--- arrayBST.h
+++ arrayBST.h
@@ -10,7 +10,7 @@ public:
 	ArrayBST(int root = 0) :root(root){}
 	bool isEmpty();
 	void addBst(int key, int index = 1);
-	void removeBst(int key, int index = 1);
+	//void removeBst(int key, int index = 1);
 	bool searchBst(int key, int index = 1);
 	void printBST(int value = 1);
 	int largest(int index);
