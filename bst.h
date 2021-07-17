9e935556964d8c8a311a34765d6c67b219677af0
Ankush Niroula <ankushniroulaa@gmail.com>
2021-07-17 21:43:32 +0545

    base class

--- bst.h
+++ bst.h
@@ -1,1 +1,9 @@
 #pragma once
+
+class Abstractbst {
+public:
+	virtual bool isEmpty() = 0;
+	virtual void addBst(int key, int index) = 0;
+	virtual void removeBst(int key, int index) = 0;
+	virtual bool searchBst(int key, int index) = 0;
+};
