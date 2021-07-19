b38ec81d937af259f4631335536de23121a18a4f
Ankush Niroula <ankushniroulaa@gmail.com>
2021-07-19 21:35:53 +0545

    Merged with arrayBST

--- bst.h
+++ bst.h
@@ -3,15 +3,8 @@
 class Abstractbst {
 public:
 	virtual bool isEmpty() = 0;
-<<<<<<< HEAD
-	virtual void addBst(int key, int index) = 0;
-	virtual void removeBst(int key, int index) = 0;
-	virtual bool searchBst(int key, int index) = 0;
-};
-=======
 	virtual void addBst(int data = 0, int index = 0 ) = 0;
 	//remove method took different parameteres in array and linked list representation, so we did not include it in abstract class
 	//virtual void removeBst(int data = 0, int index = 0) = 0;  
 	virtual bool searchBst(int data = 0, int index = 0) = 0;
 };
->>>>>>> 504937b3a7d14c265772f486bc95a32e3e527a93
